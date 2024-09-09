e38c577481662e1f6b9aa813146132f235fa3569
Jay <jay.sanjay.karia@gmail.com>
2024-09-09 22:19:28 +0530

    chore: add conditions

--- src/utils/basic/convertColor.ts
+++ src/utils/basic/convertColor.ts
@@ -4,12 +4,31 @@ import { Format } from "../../types/format";
 export function convertColor(color: string, to: Format): string {
   const colorFormat = detectFormat(color);
 
-  if (!colorFormat) {
+  if (!colorFormat || !detectFormat(to)) {
     throw new Error("Invalid color format.");
   }
 
   if (colorFormat === to) {
     return color;
+  }
+
+  // hex to rgb
+  if (colorFormat === "hex" && to === "rgb") {
+  }
+  // hex to hsl
+  if (colorFormat === "hex" && to === "hsl") {
+  }
+  // rgb to hex
+  if (colorFormat === "rgb" && to === "hex") {
+  }
+  // rgb to hsl
+  if (colorFormat === "rgb" && to === "hsl") {
+  }
+  // hsl to hex
+  if (colorFormat === "hsl" && to === "hex") {
+  }
+  // hsl to rgb
+  if (colorFormat === "hsl" && to === "rgb") {
   }
 
   return "";
