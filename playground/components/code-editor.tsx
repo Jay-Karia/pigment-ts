9de7f8d50ede35c2007c88e0d542a0652fc97f4a
Jay <jay.sanjay.karia@gmail.com>
2024-11-06 08:44:26 +0530

    fix: ts error

--- playground/components/code-editor.tsx
+++ playground/components/code-editor.tsx
@@ -6,37 +6,41 @@ declare global {
   }
 }
 
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useCodeStore } from "@/store/code";
-import Editor, { monaco } from "@monaco-editor/react";
+import Editor from "@monaco-editor/react";
 import PigmentTS from "pigment-ts";
+import * as monaco from "monaco-editor";
 
 export function CodeEditor() {
   const code = useCodeStore(state => state.code);
-  const setCode = useCodeStore(state => state.updateCode)
+  const setCode = useCodeStore(state => state.updateCode);
 
-  const editorRef = React.useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+  const editorRef = React.useRef<monaco.editor.IStandaloneCodeEditor | null>(
+    null
+  );
 
-  function handleEditorDidMount(editor: monaco.editor.IStandaloneCodeEditor, monaco: typeof import("monaco-editor")) {
+  function handleEditorDidMount(
+    editor: monaco.editor.IStandaloneCodeEditor,
+    monaco: typeof import("monaco-editor")
+  ) {
     editorRef.current = editor;
     monaco.languages.typescript.javascriptDefaults.addExtraLib(
       `declare const PigmentTS: typeof import('pigment-ts');`,
-      'ts:pigment-ts.d.ts'
+      "ts:pigment-ts.d.ts"
     );
   }
 
   function handleEditorChange() {
-    if (editorRef.current === null)
-      setCode("")
-    else
-      setCode(editorRef.current.getValue())
+    if (editorRef.current === null) setCode("");
+    else setCode(editorRef.current.getValue());
   }
 
   useEffect(() => {
     if (typeof window !== "undefined") {
       window.PigmentTS = PigmentTS;
     }
-  }, [])
+  }, []);
 
   return (
     <div className="w-full p-4 bg-zinc-900">
@@ -48,7 +52,6 @@ export function CodeEditor() {
         onChange={handleEditorChange}
         theme="vs-dark"
       />
-
     </div>
   );
 }
