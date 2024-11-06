0f3d2b7f23296e1655ed0fbb8be4cfe9788e765f
Jay <jay.sanjay.karia@gmail.com>
2024-11-06 14:40:56 +0530

    fix: server error

--- playground/actions/run-code.ts
+++ playground/actions/run-code.ts
@@ -2,25 +2,22 @@
 
 import axios from "axios"
 import { LANGUAGE_INFO } from "@/constants";
-import fs from "fs"
-import path from "path";
+import {libraryCode64} from "@/library-code"
 
 const API = axios.create({
   baseURL: "https://emkc.org/api/v2/piston",
 });
 
-
-
 export default async function runCode(code: string) {
-  const libraryPath = path.resolve(process.cwd(), 'minified-lib.js');
-  const libraryCode = fs.readFileSync(libraryPath, 'utf-8');
+  const base64Code = libraryCode64
+  const utf8Code = Buffer.from(base64Code, "base64").toString("utf8")
 
   const response = await API.post("/execute", {
     language: LANGUAGE_INFO.language,
     version: LANGUAGE_INFO.version,
     files: [
       {
-        content: libraryCode + code,
+        content: utf8Code + code,
       },
     ],
   });
