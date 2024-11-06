aa91985e6fbae89629b20f284d4df0cbf627f449
Jay <jay.sanjay.karia@gmail.com>
2024-11-06 12:27:33 +0530

    fix: new line in console

--- playground/components/console.tsx
+++ playground/components/console.tsx
@@ -13,10 +13,21 @@ const geistSans = localFont({
 export function Console() {
   const output = useOutputStore(state => state.output)
 
+  function handleNewLine(output: string) {
+    return output.split("\n").map((line, index) => {
+      return (
+        <div key={index} className="flex items-center">
+          <span className="text-neutral-500">{">"}</span>
+          <span className="ml-2">{line}</span>
+        </div>
+      );
+    });
+  }
+
   return (
     <div className="w-full h-full p-4 bg-neutral-900 text-white">
       <div className={cn("h-full bg-neutral-800 p-4", geistSans.className)}>
-        {output}
+        {handleNewLine(output)}
       </div>
     </div>
   );
