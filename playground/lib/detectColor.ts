74a53ae135afb6820029119e6c1ec0e99603127e
jay.sanjay.karia <jay.sanjay.karia@gmail.com>
2024-11-16 13:23:53 +0000

    fix: hsl colored text

--- playground/lib/detectColor.ts
+++ playground/lib/detectColor.ts
@@ -6,7 +6,7 @@ export function detectColor(line: string) {
   const regex = {
     rgb: /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/,
     hex: /#[0-9A-F]{6}/i,
-    hsl: /hsl\((\d+),\s*([\d.]+)%,\s*([\d.]+)%\)/g,
+    hsl: /hsl\((\d+),\s*([\d.]+)%,\s*([\d.]+)%\)/,
     tw: /(?:$|^|)(slate-|gray-|zinc-|neutral-|stone-|red-|orange-|amber-|yellow-|lime-|green-|emerald-|teal-|cyan-|sky-|blue-|indigo-|violet-|purple-|fuchsia-|pink-|rose-|white|black)(50|100|200|300|400|500|600|700|800|900|950|)(?:$|^|)/gi,
     rgba: /rgba\((\d{1,3}), (\d{1,3}), (\d{1,3}), ([0-1](\.\d+)?)\)/,
     hsla: /hsla\((\d+),\s*([\d.]+)%,\s*([\d.]+)%, ([0-1](\.\d+)?)\)/,
