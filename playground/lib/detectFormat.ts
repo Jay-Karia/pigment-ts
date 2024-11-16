08dc14c83daca073252901ed45136fe6eebbd3f0
jay.sanjay.karia <jay.sanjay.karia@gmail.com>
2024-11-16 13:55:22 +0000

    fix: colored tailwind classes

---
+++ playground/lib/detectFormat.ts
@@ -0,0 +1,18 @@
+import { Format } from "@/types/format";
+
+export function detectFormat(rawString: string): Format | null {
+  const formats = ["rgb", "hex", "hsl", "tw", "rgba", "hsla"] as Format[];
+  const regex = {
+    rgb: /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/,
+    hex: /^#[0-9A-F]{6}$/i,
+    hsl: /hsl\((\d+),\s*([\d.]+)%,\s*([\d.]+)%\)/g,
+    tw: /(?:$|^|)(slate-|gray-|zinc-|neutral-|stone-|red-|orange-|amber-|yellow-|lime-|green-|emerald-|teal-|cyan-|sky-|blue-|indigo-|violet-|purple-|fuchsia-|pink-|rose-|white|black)(50|100|200|300|400|500|600|700|800|900|950|)(?:$|^|)/gi,
+    rgba: /rgba\((\d{1,3}), (\d{1,3}), (\d{1,3}), ([0-1](\.\d+)?)\)/,
+    hsla: /hsla\((\d+),\s*([\d.]+)%,\s*([\d.]+)%, ([0-1](\.\d+)?)\)/,
+  } as Record<string, RegExp>;
+
+  for (const format of formats)
+    if (regex[format].test(rawString)) return format as Format;
+
+  return null;
+}
